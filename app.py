f5d0317e5e2c54b434165ac6cf03e3d971c30069
ahmetugsuz <80051605+ahmetugsuz@users.noreply.github.com>
2025-01-16 06:49:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ migrate = Migrate(app, db)
 ssl_ca_certs = './ca.crt'
 
 # Redis URL from environment variable
-redis_url = os.environ.get('REDIS_URL')  # Ensure this is set correctly
+#redis_url = os.environ.get('REDIS_URL')  # Ensure this is set correctly
 
 # Initialize Redis client
 redis_client = redis.StrictRedis.from_url(
-    redis_url,
+    redis_url='rediss://:p77275872e8dc6a1296ed70f2379a3d2e7816ed21d638c976c767e714f6cab944@ec2-52-49-254-201.eu-west-1.compute.amazonaws.com:26240',
     decode_responses=True,  # Ensures Python strings are returned instead of bytes
     ssl=True,               # Enable SSL
     ssl_ca_certs=ssl_ca_certs  # Validate the server certificate
