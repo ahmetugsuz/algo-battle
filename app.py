2502cbd0e245826aee2f0ff7546b20e555a25f96
ahmetugsuz <80051605+ahmetugsuz@users.noreply.github.com>
2025-01-17 03:39:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ migrate = Migrate(app, db)
 ssl_ca_certs = './ca.crt'
 
 # Redis connection URL
-redis_url = 'redis://:password@ec2-52-49-254-201.eu-west-1.compute.amazonaws.com:26240'
+redis_url = 'redis://:p77275872e8dc6a1296ed70f2379a3d2e7816ed21d638c976c767e714f6cab944@ec2-52-49-254-201.eu-west-1.compute.amazonaws.com:26240'
 
 redis_client = redis.StrictRedis.from_url(
     redis_url,
